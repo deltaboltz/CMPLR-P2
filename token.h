a193d8b93ef24f57151ed782638af92f8a05ecbf
Connor Schultz <deltaboltz@me.com>
2021-04-11 11:53:51 -0500

    changes to token.h

--- token.h
+++ token.h
@@ -3,6 +3,7 @@
 #include <map>
 
 enum tokenID {
+    null=-1
     error=0,
     identifier=1001,
     integer=1002,
@@ -17,6 +18,7 @@ struct token {
     std::pair<int, int> pos;
     std::pair<int, int> size;
     std::map<int, std::string> idmap = {
+        {null,        "\nNULL TOKEN\n"},
         { error,      "\nSCANNER ERROR:\n"},
         { identifier, "idTK:" },
         { integer,    "numTK:" },
@@ -26,24 +28,39 @@ struct token {
     };
     token(enum tokenID tid, std::string tinst, std::pair<int, int> tpos,
         std::pair<int, int> tsize) : id(tid), instance(tinst), pos(tpos), size(tsize) {}
-    std::string toString() {
-        std::string out("");
-        std::string posString("");
-        std::string sizeString("");
-        posString += std::to_string(std::get<0>(pos)) + ":" +\
-                     std::to_string(std::get<1>(pos));
-        sizeString += std::to_string(std::get<0>(size)) + ":" +\
-                      std::to_string(std::get<1>(size));
-        if (id == error) {
-            out += idmap[id] + posString + ": " + instance;
-        } else {
-            out += idmap[id];
-            out.append(16 - out.length(), ' ');
-            out += instance;
-            out.append(28 - out.length(), ' ');
-            out += "Line " + posString;
-        }
 
+    token()
+    {
+      id = NULL;
+      instance = "";
+      pos = std::make_pair(0,0);
+      size = std::make_pair(0,0);
+    }
+
+    std::string posString()
+    {
+      std::string p("");
+
+      p+= std::to_string(std::get<0>(pos)) + ":" + std::to_string(std::get<1>(pos));
+
+      return p;
+    }
+
+    std::string toString()
+    {
+        std::string out("");
+        if (id == error)
+        {
+            out += idmap[id] + posString() + ": " + instance;
+        }
+        else if(id == null)
+        {
+          out += idmap[id];
+        }
+        else
+        {
+          out += idmap[id] + "'" + instance + "'";
+        }
         return out;
     }
 };
